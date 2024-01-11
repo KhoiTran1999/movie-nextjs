363ba763ec5d4750a90bc52e610936a313d06507
Khoitran1999 <tranquockhoi1999@gmail.com>
2024-01-11 11:44:49 +0700

    test prod 2

--- app/api/palmAi/[text]/route.tsx
+++ app/api/palmAi/[text]/route.tsx
@@ -11,12 +11,9 @@ export async function GET(req: NextRequest, res: NextApiResponse) {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  let text = "";
-  const result = await model.generateContentStream(prompt || "");
-  for await (const chunk of result.stream) {
-    const chunkText = chunk.text();
-    text += chunkText;
-  }
+  const result = await model.generateContent(prompt || "");
+  const response = await result.response;
+  const text = response.text();
 
-  return new Response("hello");
+  return new Response(text);
 }
