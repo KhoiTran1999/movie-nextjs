40e0fabc245636cd262941e4832375a318b0e695
Khoitran1999 <tranquockhoi1999@gmail.com>
2024-01-31 21:45:06 +0700

    #

--- components/detailPage/MainDetailPage.tsx
+++ components/detailPage/MainDetailPage.tsx
@@ -153,159 +153,169 @@ export default function MainDetailPage(props: MainDetailPage) {
             }}
           ></div>
 
-          <div
-            className={`m-auto w-full max-w-[1200px] px-3 py-8 backdrop-blur-sm`}
-          >
-            <div className="m-auto mt-14 w-full max-w-[700px] text-[#D1D0CF]">
-              <div className="flex items-center">
-                <div className="w-[60%]">
-                  <h1
-                    className={`${rubik.className} my-2 w-full text-2xl tracking-wider [word-spacing:5px] md:my-4  md:text-5xl`}
-                  >
-                    {movieDetail.englishName}
-                  </h1>
-                  <h2 className="font-bold">{movieDetail.vietnamName}</h2>
-                  <div className="my-4">
-                    <span>{movieDetail.producedDate.slice(0, 4)}</span>
-                    <span className="mx-4">
-                      {movieDetail.totalSeasons > 1
-                        ? `${movieDetail.totalSeasons} seasons`
-                        : movieDetail.totalEpisodes > 1
-                          ? `${movieDetail.totalEpisodes} episodes`
-                          : `${movieDetail.time} minutes`}
-                    </span>
-                    <span>
-                      {movieDetail.mark}/10{" "}
-                      <StarFilled className="text-yellow-400" />
-                    </span>
-                    <ul className="mt-2 flex flex-wrap items-center">
-                      {movieDetail.categories.map(
-                        (
-                          val: { categoryId: number; name: string },
-                          idx: number,
-                        ) => {
-                          if (idx + 1 < movieDetail.categories.length) {
+          <div className="backdrop-blur-sm">
+            <div className={`m-auto w-full max-w-[1200px] px-3 py-8`}>
+              <div className="m-auto mt-14 w-full max-w-[700px] text-[#D1D0CF]">
+                <div className="flex items-center">
+                  <div className="w-[60%]">
+                    <h1
+                      className={`${rubik.className} my-2 w-full text-2xl tracking-wider [word-spacing:5px] md:my-4  md:text-5xl`}
+                    >
+                      {movieDetail.englishName}
+                    </h1>
+                    <h2 className="font-bold">{movieDetail.vietnamName}</h2>
+                    <div className="my-4">
+                      <span>{movieDetail.producedDate.slice(0, 4)}</span>
+                      <span className="mx-4">
+                        {movieDetail.totalSeasons > 1
+                          ? `${movieDetail.totalSeasons} seasons`
+                          : movieDetail.totalEpisodes > 1
+                            ? `${movieDetail.totalEpisodes} episodes`
+                            : `${movieDetail.time} minutes`}
+                      </span>
+                      <span>
+                        {movieDetail.mark}/10{" "}
+                        <StarFilled className="text-yellow-400" />
+                      </span>
+                      <ul className="mt-2 flex flex-wrap items-center">
+                        {movieDetail.categories.map(
+                          (
+                            val: { categoryId: number; name: string },
+                            idx: number,
+                          ) => {
+                            if (idx + 1 < movieDetail.categories.length) {
+                              return (
+                                <li className="mr-2" key={val.categoryId}>
+                                  <span className="mr-2 cursor-pointer hover:text-[#E50914]">
+                                    {val.name}
+                                  </span>
+                                  <FireFilled className="text-xs text-[#E50914]" />
+                                </li>
+                              );
+                            }
                             return (
                               <li className="mr-2" key={val.categoryId}>
-                                <span className="mr-2 cursor-pointer hover:text-[#E50914]">
+                                <span className=" cursor-pointer hover:text-[#E50914]">
                                   {val.name}
                                 </span>
-                                <FireFilled className="text-xs text-[#E50914]" />
                               </li>
                             );
-                          }
-                          return (
-                            <li className="mr-2" key={val.categoryId}>
-                              <span className=" cursor-pointer hover:text-[#E50914]">
-                                {val.name}
-                              </span>
-                            </li>
-                          );
-                        },
-                      )}
-                    </ul>
+                          },
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="w-[30%] overflow-hidden">
+                    <LazyLoadImage
+                      alt="Thumbnail"
+                      src={movieDetail.thumbnail}
+                      effect="blur"
+                      loading="lazy"
+                      className="h-full w-full rounded-md object-cover"
+                      onError={(e) => {
+                        e.currentTarget.onerror = null;
+                        e.currentTarget.src = "/errorThumbnail.jpg";
+                      }}
+                    />
                   </div>
                 </div>
-                <div className="w-[30%] overflow-hidden">
-                  <LazyLoadImage
-                    alt="Thumbnail"
-                    src={movieDetail.thumbnail}
-                    effect="blur"
-                    loading="lazy"
-                    className="h-full w-full rounded-md object-cover"
-                    onError={(e) => {
-                      e.currentTarget.onerror = null;
-                      e.currentTarget.src = "/errorThumbnail.jpg";
-                    }}
-                  />
+
+                <Tabs
+                  type="card"
+                  defaultActiveKey="Description"
+                  items={[
+                    {
+                      key: "Description",
+                      label: "Description",
+                      children: movieDetail.description,
+                    },
+                    {
+                      key: "Actors",
+                      label: "Actors",
+                      children: (
+                        <Actor
+                          castCharacteries={movieDetail.castCharacteries}
+                        />
+                      ),
+                    },
+                  ]}
+                />
+                <div className="mt-10 flex items-center justify-start">
+                  <button
+                    onClick={showModal}
+                    className="mr-3 h-10 w-[121px] rounded-md bg-[red] px-1 text-white"
+                    disabled={movieDetail.totalSeasons === 0}
+                  >
+                    {movieDetail.totalSeasons > 0 ? (
+                      <>
+                        {loadingMovie ? (
+                          <i className="fa-solid fa-circle-notch mr-2 animate-spin text-xl"></i>
+                        ) : (
+                          <i className="fa-duotone fa-play mr-2 text-xl"></i>
+                        )}
+
+                        <span>Play</span>
+                      </>
+                    ) : (
+                      <span>Upcoming...</span>
+                    )}
+                  </button>
+
+                  <button
+                    onClick={handleOpenTrailerModal}
+                    className="mr-3 h-10 w-[121px] rounded-md bg-[#b2afaf2e] px-1 text-white transition-colors hover:bg-[#adaaaa64]"
+                  >
+                    <InfoCircleOutlined />
+                    <span className="ml-3">Trailer</span>
+                  </button>
+
+                  <Tooltip color="#b2afaf2e" title="Add watch list">
+                    <span
+                      className={`flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
+                    >
+                      <i className="fa-regular fa-plus text-xl"></i>
+                    </span>
+                  </Tooltip>
+
+                  <Tooltip color="#b2afaf2e" title="Like">
+                    <span
+                      className={`mx-3 flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
+                    >
+                      <i className="fa-regular fa-heart text-xl"></i>
+                    </span>
+                  </Tooltip>
+
+                  <Tooltip color="#b2afaf2e" title="Share">
+                    <span
+                      className={`flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
+                    >
+                      <i className="fa-light fa-share-from-square text-xl"></i>
+                    </span>
+                  </Tooltip>
                 </div>
               </div>
-
-              <Tabs
-                type="card"
-                defaultActiveKey="Description"
-                items={[
-                  {
-                    key: "Description",
-                    label: "Description",
-                    children: movieDetail.description,
-                  },
-                  {
-                    key: "Actors",
-                    label: "Actors",
-                    children: (
-                      <Actor castCharacteries={movieDetail.castCharacteries} />
-                    ),
-                  },
-                ]}
-              />
-              <div className="mt-10 flex items-center justify-start">
-                <button
-                  onClick={showModal}
-                  className="mr-3 h-10 w-[121px] rounded-md bg-[red] px-1 text-white"
-                  disabled={movieDetail.totalSeasons === 0}
-                >
-                  {movieDetail.totalSeasons > 0 ? (
-                    <>
-                      {loadingMovie ? (
-                        <i className="fa-solid fa-circle-notch mr-2 animate-spin text-xl"></i>
-                      ) : (
-                        <i className="fa-duotone fa-play mr-2 text-xl"></i>
-                      )}
-
-                      <span>Play</span>
-                    </>
-                  ) : (
-                    <span>Upcoming...</span>
+              <div className="mt-8">
+                <h3 className="text-2xl font-bold text-red-700">
+                  Recommended Movie
+                </h3>
+                <List
+                  dataSource={recommendedMovie}
+                  grid={{
+                    xs: 3,
+                    sm: 3,
+                    md: 3,
+                    lg: 4,
+                    xl: 5,
+                    xxl: 5,
+                    gutter: 12,
+                  }}
+                  renderItem={(val: MovieType, idx: number) => (
+                    <div onClick={() => setIsSkeleton(true)}>
+                      <CardMovie val={val} />
+                    </div>
                   )}
-                </button>
-
-                <button
-                  onClick={handleOpenTrailerModal}
-                  className="mr-3 h-10 w-[121px] rounded-md bg-[#b2afaf2e] px-1 text-white transition-colors hover:bg-[#adaaaa64]"
-                >
-                  <InfoCircleOutlined />
-                  <span className="ml-3">Trailer</span>
-                </button>
-
-                <Tooltip color="#b2afaf2e" title="Add watch list">
-                  <span
-                    className={`flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
-                  >
-                    <i className="fa-regular fa-plus text-xl"></i>
-                  </span>
-                </Tooltip>
-
-                <Tooltip color="#b2afaf2e" title="Like">
-                  <span
-                    className={`mx-3 flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
-                  >
-                    <i className="fa-regular fa-heart text-xl"></i>
-                  </span>
-                </Tooltip>
-
-                <Tooltip color="#b2afaf2e" title="Share">
-                  <span
-                    className={`flex h-11 w-11 cursor-pointer items-center justify-center rounded-full  bg-[#b2afaf2e] p-3 transition-colors hover:bg-[#adaaaa64]`}
-                  >
-                    <i className="fa-light fa-share-from-square text-xl"></i>
-                  </span>
-                </Tooltip>
+                />
               </div>
-            </div>
-            <div className="mt-8">
-              <h3 className="text-2xl font-bold text-red-700">
-                Recommended Movie
-              </h3>
-              <List
-                dataSource={recommendedMovie}
-                grid={{ xs: 3, sm: 3, md: 3, lg: 4, xl: 5, xxl: 5, gutter: 12 }}
-                renderItem={(val: MovieType, idx: number) => (
-                  <div onClick={() => setIsSkeleton(true)}>
-                    <CardMovie val={val} />
-                  </div>
-                )}
-              />
             </div>
           </div>
           <Modal
