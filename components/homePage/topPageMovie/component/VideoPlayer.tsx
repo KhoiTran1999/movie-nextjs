032e8bd49fc3b664df9b7449eb311eba993fb46a
KhoiTran1999 <tranquockhoi1999@gmail.com>
2024-02-24 16:39:48 +0700

    Prevent VideoPlayer play in TopPage

--- components/homePage/topPageMovie/component/VideoPlayer.tsx
+++ components/homePage/topPageMovie/component/VideoPlayer.tsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 const ReactPlayer = dynamic(() => import("react-player/youtube"), {
   ssr: false,
 });
@@ -20,24 +21,35 @@ interface VideoType {
 }
 
 const VideoPlayer = ({ previewMovie }: VideoType) => {
+  const [screenWidth, setScreenWidth] = useState<number>(0);
+
+  useEffect(() => {
+    if (screen) {
+      setScreenWidth(screen.width);
+    }
+  }, []);
+  console.log(screenWidth);
+
   return (
     <div className="h-[50svh] sm:h-[70svh]">
       <div className="hidden sm:block">
-        <ReactPlayer
-          url={previewMovie.trailer}
-          loop
-          muted
-          playing
-          width={"100svw"}
-          height={"80svh"}
-          style={{
-            backgroundSize: "contain",
-            backgroundPosition: "center center",
-            filter: "brightness(.7)",
-            scale: 1.45,
-            backgroundColor: "black",
-          }}
-        />
+        {screenWidth > 640 && (
+          <ReactPlayer
+            url={previewMovie.trailer}
+            loop
+            muted
+            playing
+            width={"100svw"}
+            height={"80svh"}
+            style={{
+              backgroundSize: "contain",
+              backgroundPosition: "center center",
+              filter: "brightness(.7)",
+              scale: 1.45,
+              backgroundColor: "black",
+            }}
+          />
+        )}
       </div>
       <div className="relative inline-block h-full w-full sm:hidden">
         <img
