18a7c6e1e2665ddc474db559c7f7319e293e82e9
Khoitran1999 <tranquockhoi1999@gmail.com>
2024-02-27 08:46:39 +0700

    Fix Error display user Nav in Admin Page

--- components/homePage/navigationMovie/NavigationMovie.tsx
+++ components/homePage/navigationMovie/NavigationMovie.tsx
@@ -1,35 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import NavigationFeature from "./NavigationFeature";
 import User from "./User";
 import { SuspenseComp } from "@/components/wrapper/SuspenseComp";
+import { usePathname } from "next/navigation";
 
 const NavigationMovie = () => {
+  const pathname = usePathname();
+
   return (
-    <div
-      className={`${
-        true
-          ? "bg-gradient-to-b from-black to-transparent backdrop-blur-none"
-          : "bg-black/20 backdrop-blur-lg"
-      } fixed left-0 top-0  z-[100] w-full px-4 pt-1 transition-all lg:px-12`}
-    >
-      <div className="m-auto flex w-full max-w-[1200px] items-center justify-between ">
-        <Link href="/">
-          <Image src="/logo.png" alt="logo" width={100} height={50} />
-        </Link>
-        <div className="hidden md:block">
-          <SuspenseComp>
-            <NavigationFeature />
-          </SuspenseComp>
+    <>
+      {!pathname.includes("admin") && (
+        <div
+          className={`${
+            true
+              ? "bg-gradient-to-b from-black to-transparent backdrop-blur-none"
+              : "bg-black/20 backdrop-blur-lg"
+          } fixed left-0 top-0  z-[100] w-full px-4 pt-1 transition-all lg:px-12`}
+        >
+          <div className="m-auto flex w-full max-w-[1200px] items-center justify-between ">
+            <Link href="/">
+              <Image src="/logo.png" alt="logo" width={100} height={50} />
+            </Link>
+            <div className="hidden md:block">
+              <SuspenseComp>
+                <NavigationFeature />
+              </SuspenseComp>
+            </div>
+            <div className="flex items-center justify-end pr-5">
+              <Link href={"/search"}>
+                <i className="fa-regular fa-magnifying-glass cursor-pointer text-xl text-[#D1D0CF]"></i>
+              </Link>
+              <User />
+            </div>
+          </div>
         </div>
-        <div className="flex items-center justify-end pr-5">
-          <Link href={"/search"}>
-            <i className="fa-regular fa-magnifying-glass cursor-pointer text-xl text-[#D1D0CF]"></i>
-          </Link>
-          <User />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
