2d98fdc6da7b5c1f6d29c6e2c51b76ab8b3154d3
KhoiTran1999 <tranquockhoi1999@gmail.com>
2024-03-08 10:43:03 +0700

    Add Tour, Change Public Route for Login

--- components/homePage/navigationMovie/User.tsx
+++ components/homePage/navigationMovie/User.tsx
@@ -1,14 +1,46 @@
 "use client";
 
-import React from "react";
-import { UserButton } from "@clerk/nextjs";
-import { useUser } from "@clerk/nextjs";
-import { Button } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { Button, Tour } from "antd";
+import type { TourProps } from "antd";
+import { CloseOutlined } from "@ant-design/icons";
 import { usePathname } from "next/navigation";
 
 const User = () => {
   const pathname = usePathname();
+
   const { user, isLoaded, isSignedIn } = useUser();
+
+  const loginRef = useRef(null);
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  const steps: TourProps["steps"] = [
+    {
+      title: "Login with Microsoft Account",
+      description: "You're gonna have a better movie watching experience",
+      cover: (
+        <img
+          className="rounded-md"
+          alt="Login with Microsoft"
+          src="/loginWithMicrosoft.jpg"
+        />
+      ),
+      target: () => loginRef.current,
+    },
+  ];
+
+  useEffect(() => {
+    const isNew = localStorage.getItem("visit") == null;
+    if (isNew) {
+      localStorage.setItem("visit", "TRUE");
+      setOpen(true);
+      //It's a new user
+    } else {
+      //It's not a new user
+    }
+  }, []);
 
   return (
     <div className="ml-5">
@@ -17,12 +49,30 @@ const User = () => {
       ) : (
         <>
           {!pathname.includes("sign-in") && (
-            <Button href={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}>
+            <Button
+              ref={loginRef}
+              href={process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}
+            >
               Login
             </Button>
           )}
         </>
       )}
+      <Tour
+        open={open}
+        onClose={() => setOpen(false)}
+        steps={steps}
+        indicatorsRender={(current, total) => (
+          <span>
+            {current + 1} / {total}
+          </span>
+        )}
+        animated={true}
+        mask={{ color: "#ffffff3e" }}
+        closeIcon={
+          <CloseOutlined className="rounded-full bg-[#ffffff3e] p-1" />
+        }
+      />
     </div>
   );
 };
