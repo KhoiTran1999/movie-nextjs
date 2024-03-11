abf6cbcb8ae91721662b4d69b057b023f50cf32e
KhoiTran1999 <tranquockhoi1999@gmail.com>
2024-03-11 13:32:33 +0700

    Fix search Movie in 2

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,3 @@
-"use server";
-
 import NavigationMovie from "@/components/homePage/navigationMovie/NavigationMovie";
 import SearchMovie from "@/components/searchPage/SearchMovie";
 import { MovieType } from "@/types";
@@ -8,14 +6,20 @@ const Search = async (props: any) => {
   const text = props?.searchParams?.text || "";
   const page = props?.searchParams?.page || 1;
 
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}Movies?key=${text}&page=${page}&eachPage=10`,
-    {
-      next: { revalidate: 86400 },
-    },
-  );
-  const totalPage = Number(res.headers.get("x-total-page"));
-  const movieSearch: MovieType[] = await res.json();
+  let totalPage: number = 1;
+  let movieSearch: MovieType[] = [];
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/Movies?key=${text}&page=${page}&eachPage=10`,
+      {
+        next: { revalidate: 86400 },
+      },
+    );
+    totalPage = Number(res.headers.get("x-total-page"));
+    movieSearch = await res.json();
+  } catch (error) {
+    console.log(error);
+  }
 
   return (
     <div>
--- components/homePage/navigationMovie/NavigationMovie.tsx
+++ components/homePage/navigationMovie/NavigationMovie.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 import NavigationFeature from "./NavigationFeature";
 import User from "./User";
