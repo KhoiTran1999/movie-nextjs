4577d1c2a282d907c18845f7f1d557d815fcaa42
KhoiTran1999 <tranquockhoi1999@gmail.com>
2024-03-11 12:58:47 +0700

    Fix search Movie

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,29 @@
 import NavigationMovie from "@/components/homePage/navigationMovie/NavigationMovie";
 import SearchMovie from "@/components/searchPage/SearchMovie";
+import { MovieType } from "@/types";
 
-const Search = async () => {
+const Search = async (props: any) => {
+  const text = props?.searchParams?.text || "";
+  const page = props?.searchParams?.page || 1;
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}Movies?key=${encodeURIComponent(text)}&page=${page}&eachPage=10`,
+    {
+      next: { revalidate: 86400 },
+    },
+  );
+  const totalPage = Number(res.headers.get("x-total-page"));
+  const movieSearch: MovieType[] = await res.json();
+
   return (
     <div>
       <NavigationMovie />
-      <SearchMovie />
+      <SearchMovie
+        movieSearch={text ? movieSearch : []}
+        page={Number(page)}
+        totalPage={totalPage}
+        text={text}
+      />
     </div>
   );
 };
--- components/searchPage/SearchMovie.tsx
+++ components/searchPage/SearchMovie.tsx
@@ -1,115 +1,76 @@
 "use client";
 
 import { MovieType } from "@/types";
-import { AutoComplete, List } from "antd";
+import { AutoComplete, Button } from "antd";
 import debounce from "lodash.debounce";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CardMovie from "../homePage/cardSlider/CardMovieClient";
-import { useInView } from "react-intersection-observer";
 import Image from "next/image";
-import { getSearchMovieListAction } from "../actions";
+import { RightOutlined, LeftOutlined } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
 
-const SearchMovie = () => {
-  const [ref, inView] = useInView();
+interface SearchMovieType {
+  movieSearch: MovieType[];
+  totalPage: number;
+  page: number;
+  text: string;
+}
 
-  const [movieSearch, setMovieSearch] = useState<MovieType[]>([]);
-  const [valueSearch, setValueSearch] = useState<string>("");
-  const [page, setPage] = useState<number>(1);
-  const [totalElement, setTotalElement] = useState<number>(0);
-  const [isLoadingSearch, setIsloadingSearch] = useState<boolean>(false);
+const SearchMovie = (props: SearchMovieType) => {
+  const { movieSearch, totalPage, page, text } = props;
 
-  useEffect(() => {
-    if (inView) {
-      const loadMorePage = async () => {
-        const next: number = page + 1;
-        const movieList = await getSearchMovieListAction(valueSearch, next);
-        setMovieSearch((prev: MovieType[] | undefined) => [
-          ...(prev?.length ? prev : []),
-          ...movieList.data,
-        ]);
-        setPage(next);
-      };
-      loadMorePage();
-    }
-  }, [inView]);
+  const router = useRouter();
+
+  const [valueSearch, setValueSearch] = useState<string>(text || "");
 
   const handleSearch = async (text: string) => {
-    setIsloadingSearch(true);
-    if (!text) {
-      setPage(1);
-      setTotalElement(0);
-      setValueSearch("");
-      setIsloadingSearch(false);
-      return setMovieSearch([]);
-    }
-    const movieList = await getSearchMovieListAction(text);
-    setMovieSearch(movieList.data);
     setValueSearch(text);
-    setTotalElement(movieList.totalItems);
-    setPage(1);
-    setIsloadingSearch(false);
+    router.push(`/search?text=${text}&page=1&eachPage=10`);
   };
 
   return (
-    <div className="m-auto mt-20 w-full max-w-[1200px] animate-opacityAnimated">
+    <div className="m-auto mt-20 w-full animate-opacityAnimated">
       <div className="flex justify-center">
         <AutoComplete
           style={{ width: 200 }}
           placeholder="What is your favorite movie?"
           onSearch={debounce(handleSearch, 1000)}
           autoFocus
+          defaultValue={text}
         />
       </div>
-      {isLoadingSearch ? (
-        <div className="m-auto mt-14 w-full max-w-[700px] px-3">
-          <div className="flex items-center justify-center">
-            <div className="mr-3 h-[200px] w-[150px] animate-pulse rounded-md bg-[#ffffff3f]"></div>
-            <div className="mr-3 h-[200px] w-[150px] animate-pulse rounded-md bg-[#ffffff3f]"></div>
-            <div className="h-[200px] w-[150px] animate-pulse rounded-md bg-[#ffffff3f]"></div>
-          </div>
-        </div>
-      ) : (
-        <>
-          {movieSearch.length === 0 ? (
-            <div className="mt-7 flex h-full w-full justify-center">
-              <div className="relative aspect-[5/3] w-[400px]">
-                {valueSearch ? (
-                  <Image
-                    src={"/EmptyMovie.png"}
-                    alt="EmptyMovie"
-                    fill
-                    priority
-                    className="rounded object-cover"
-                    quality={100}
-                    sizes="(min-width: 1024px) 100vw, (min-width: 625px) 30vw, 40vw"
-                  />
-                ) : (
-                  <Image
-                    src={"/SearchMovie.png"}
-                    alt="SearchMovie"
-                    fill
-                    priority
-                    className="rounded object-cover"
-                    quality={100}
-                    sizes="(min-width: 1024px) 100vw, (min-width: 625px) 30vw, 40vw"
-                  />
-                )}
-              </div>
+      <>
+        {movieSearch?.length === 0 ? (
+          <div className="mt-7 flex h-full w-full justify-center">
+            <div className="relative aspect-[5/3] w-[400px]">
+              {valueSearch ? (
+                <Image
+                  src={"/EmptyMovie.png"}
+                  alt="EmptyMovie"
+                  fill
+                  priority
+                  className="rounded object-cover"
+                  quality={100}
+                  sizes="(min-width: 1024px) 100vw, (min-width: 625px) 30vw, 40vw"
+                />
+              ) : (
+                <Image
+                  src={"/SearchMovie.png"}
+                  alt="SearchMovie"
+                  fill
+                  priority
+                  className="rounded object-cover"
+                  quality={100}
+                  sizes="(min-width: 1024px) 100vw, (min-width: 625px) 30vw, 40vw"
+                />
+              )}
             </div>
-          ) : (
-            <List
-              dataSource={movieSearch}
-              grid={{
-                xs: 3,
-                sm: 3,
-                md: 3,
-                lg: 4,
-                xl: 5,
-                xxl: 5,
-                gutter: 12,
-              }}
-              renderItem={(val: MovieType, idx: number) => (
-                <div className="mt-7">
+          </div>
+        ) : (
+          <div>
+            <div className="grid grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
+              {movieSearch?.map((val: MovieType, idx: number) => (
+                <div key={idx}>
                   <CardMovie
                     englishName={val.englishName}
                     vietnamName={val.vietnamName}
@@ -120,20 +81,34 @@ const SearchMovie = () => {
                     totalSeasons={val.totalSeasons}
                   />
                 </div>
-              )}
-            />
-          )}
-        </>
-      )}
-
-      {totalElement > movieSearch.length && movieSearch.length > 0 && (
-        <div className="mt-6 flex justify-center py-3">
-          <i
-            ref={ref}
-            className="fa-duotone fa-spinner-third animate-spin text-5xl text-[red]"
-          ></i>
-        </div>
-      )}
+              ))}
+            </div>{" "}
+            <div className="mr-5 flex items-center justify-end">
+              <Button
+                disabled={page === 1}
+                href={`/search?text=${valueSearch}&page=${page - 1}&eachPage=10`}
+                type="text"
+              >
+                <div>
+                  <LeftOutlined />
+                </div>
+              </Button>
+              <span className="mx-5">
+                {page} / {totalPage}
+              </span>
+              <Button
+                disabled={page === totalPage}
+                href={`/search?text=${valueSearch}&page=${page + 1}&eachPage=10`}
+                type="text"
+              >
+                <div>
+                  <RightOutlined />
+                </div>
+              </Button>
+            </div>
+          </div>
+        )}
+      </>
     </div>
   );
 };
